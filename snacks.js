1493c13636f2e8273dbd3d28daebfb1ab33fca8a
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-12-09 14:12:33 +0100

    Snack 3 and 4 completed

--- snacks.js
+++ snacks.js
@@ -2,8 +2,20 @@ function getInitials(name) {
     return name.charAt(0);
 }
 
-function createSlug(string) {
+function createSlug1(string) {
     return string.split(' ').join('').toLowerCase();
 }
 
-module.exports = { getInitials, createSlug }
+function average(numArray) {
+    const somma = numArray.reduce(
+        (acc, currentValue) => acc + currentValue,
+        0,
+    );
+    return somma / numArray.length
+}
+
+function createSlug2(stringa) {
+    return stringa.split(' ').join('-').toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+module.exports = { getInitials, createSlug1, average, createSlug2 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require('./snacks.js')
+const { getInitials, average, createSlug1, createSlug2 } = require('./snacks.js')
 
 // Snack 1
 test('"La funzione getInitials restituisce le iniziali di un nome completo.', () => {
@@ -8,6 +8,18 @@ test('"La funzione getInitials restituisce le iniziali di un nome completo.', ()
 
 // Snack 2
 test('La funzione createSlug restituisce una stringa in lowercase.', () => {
-    expect(createSlug('Michele Bozzuto')).toBe('michelebozzuto');
-    expect(createSlug('Francesco')).toBe('francesco');
+    expect(createSlug1('Michele Bozzuto')).toBe('michelebozzuto');
+    expect(createSlug1('Francesco')).toBe('francesco');
+})
+
+// Snack 3
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+    expect(average([2, 4, 6])).toBe(4);
+    expect(average([10, 8])).toBe(9);
+})
+
+// Snack 4
+test('La funzione createSlug sostituisce gli spazi con -.', () => {
+    expect(createSlug2('Questo è un test')).toBe('questo-e-un-test');
+    expect(createSlug2('Prova 2')).toBe('prova-2');
 })
